e6d3eb65d73e74443641d779fecd580c87f01648
Gabriel Fernandes <fernandes.gabrielgon@gmail.com>
2025-02-12 17:10:21 -0300

    Configuracao Locale e TimeZone API

---
+++ cabeleila_leila_API/src/main/java/com/gabrielFernandes/cabeleila_leila/CabeleilaLeilaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielFernandes.cabeleila_leila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeleilaLeilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabeleilaLeilaApplication.class, args);
+	}
+
+}
---
+++ cabeleila_leila_API/src/test/java/com/gabrielFernandes/cabeleila_leila/CabeleilaLeilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielFernandes.cabeleila_leila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeleilaLeilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
